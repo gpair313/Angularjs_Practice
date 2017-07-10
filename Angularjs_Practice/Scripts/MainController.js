0737d27ee657cf1cb9a885a3f6e6d6192455b011
GriffinPair <griffin.pair@fisglobal.com>
2017-07-10 10:02:51 -0500

    Add project files.

---
+++ Angularjs_Practice/Scripts/MainController.js
@@ -0,0 +1,56 @@
+﻿// Code goes here
+(function () {
+
+    var app = angular.module("gitHubViewer", []);
+
+    var MainController = function ($scope, $http, $interval) {
+
+        var onUserComplete = function (response) {
+
+            $scope.user = response.data;
+
+            $http.get($scope.user.repos_url)
+                .then(onRepos)
+
+        };
+
+        var onRepos = function (response) {
+
+            $scope.repos = response.data;
+
+        };
+
+        var decrementCountdown = function () {
+            $scope.countdown -= 1;
+            if ($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
+            //if($)
+        };
+
+        var startCountdown = function () {
+            $interval(decrementCountdown, 1000, $scope.countdown);
+        };
+
+        $scope.search = function (username) {
+
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete);
+
+        };
+
+        $scope.username = "angular";
+
+        $scope.message = "GitHub Viewer";
+
+        $scope.repoSortOrder = "-stargazers_count";
+
+        $scope.countdown = 5;
+
+        startCountdown();
+
+    };
+
+    app.controller("MainController", ["$scope", "$http", "$interval"]);
+
+}());
